5ac67b420490db578b7ca56f98f4f5eec6bdab01
Elkin Rativa <elkinming@gmail.com>
2023-01-26 20:05:45 +0900

    feat: commit from branch

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
-        self.assertTrue('FOo'.isupper())
+        self.assertTrue('FOoo'.isupper())
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
